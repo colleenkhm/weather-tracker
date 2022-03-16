b4a843dd24ffaee77bfb9bd0c2a369d973971008
Colleen Murray <colleenkhm@gmail.com>
2022-03-16 09:19:37 -0500

    js pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// function to search for a city
+
+// function to add city to search history
+
+// function to present city name, date, icon of weather, temperature, humidity, windspeed, and uv index
+
+// function to make uv index change colors depending on conditions (favorable, moderate, severe)
+
+// function to display 5-day forecast of future conditions (date, icon of weather, temp, wind speed, humidity)
+
+// function to allow user to click items from search history and display weather
