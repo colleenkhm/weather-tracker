ac4c9a1de745b7f7c35d8ab45438b0f43c73d1e0
Colleen Murray <colleenkhm@gmail.com>
2022-03-16 19:06:07 -0500

    js to fetch city and lat/long coordinates from weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+var apiKey = '920955b96df093ea34bbb2cb68023416';
+// var currentWeather = 
+$(document).ready(function(){
+ var city = "springfield"
+    var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+// function to fetch weather API
+fetch(queryURL)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
+    return data.coord;
+})
+.then(function(coord){
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&units=imperial&appid=${apiKey}`)
+})
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data)
+})
+})
+
 // function to search for a city
 
 // function to add city to search history
