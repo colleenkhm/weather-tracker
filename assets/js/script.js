d0f9df744efecacda784602edbec01d60a3753c0
Colleen Murray <colleenkhm@gmail.com>
2022-03-20 20:08:01 -0500

    draft of search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 const apiKey = '920955b96df093ea34bbb2cb68023416';
+
 // var currentWeather = 
 $(document).ready(function(){
- var city = "portland"
+// function to search for a city
+const searchInput = document.querySelector(#searchInput.value);
+// make sure the "click" is for the search button
+input.addEventListener("click", refreshData)
+var city = searchInput;
     var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
 // function to fetch weather API
 fetch(queryURL)
 .then(function(response){
@@ -29,12 +33,17 @@ fetch(queryURL)
 var forecastFiveDayArr = [
 {
 //var date = fetch
+//var icon = (if/else ifs from fetched temp data)
+//var temp = fetch
+// var wind = fetch
+// var humidity = fetch
+// can do in one call, just need to specify what data is being fetched in that call and then create objects for each date from it
 }
 ];
 // populating weather cards to display 5-day forecast
 // might need to do multiple but there could be a way to use this format only once depending on how I populate the variables
 
-// returning data to cycle through for loop
+// returning data to cycle through for loop (can't figure out if this needs to be contained within a function or not)
 ${forecastFiveDayArr.map(function(weather) {
     return weather;
 }).join('')}
@@ -47,10 +56,9 @@ document.getElementById(i).innerHTML = `<h2>${date}</h2>
 <p>${humidity}</p>`
 }
 }
-// function to search for a city
 
 // function to add city to search history
-
+// populate the aside #recentSearches <div>
 // function to present city name, date, icon of weather, temperature, humidity, windspeed, and uv index
 
 // function to make uv index change colors depending on conditions (favorable, moderate, severe)
